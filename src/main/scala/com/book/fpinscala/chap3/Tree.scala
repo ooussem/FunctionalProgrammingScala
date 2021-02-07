3b13bdeeccd37f9dc334335bbaa872321d019d19
ooussem <oussem93@hotmail.fr>
2021-02-07 16:59:57 +0100

    add 3.27

--- src/main/scala/com/book/fpinscala/chap3/Tree.scala
+++ src/main/scala/com/book/fpinscala/chap3/Tree.scala
@@ -3,43 +3,63 @@ package com.book.fpinscala.chap3
 import scala.annotation.tailrec
 
 trait Tree[+A]
-case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+case class Leaf[A](value: A) extends Tree[A]
 
 object Tree {
-  // TODO
-//  def apply[A](xs: A*): Tree[Any] = {
-//    if(xs.isEmpty) Tree[Nothing]
-//    else {
-//      xs match {
-//        case Leaf(v) => Leaf(v)
-//        case Branch(l, r) => apply()
-//      }
-//    }
-//  }
+  // TODO: apply function
 
 
-// exo 3.25
-  def size[A](tree: Tree[A], acc: Int): Int = {
-    @tailrec
-    def go(tree: Tree[A], init: Int) : Int= {
-      tree match {
-        case Branch(l, _) => go(l, init + 1)
-        case Branch(_, r) => go(r, init + 1)
-        case Branch(l, r) => size(r, init + 1)
-        case Leaf(_) => init + 1
-      }
+  // exo 3.25
+  def size[A](tree: Tree[A]): Int = {
+    tree match {
+      case Branch(left, right) => 1 + size(left) + size(right)
+      case Leaf(_) => 1
     }
-    go(tree, acc)
   }
 
-  // TODO: 3.26
-  // TODO: 3.27
+  // exo 3.26
+  def maxInTree(tree: Tree[Int]): Int = {
+    tree match {
+      case Leaf(numb) => numb
+      case Branch(left, right) => Integer.max(maxInTree(left), maxInTree(right))
+    }
+  }
+
+  // exo 3.27
+  def depth[A](tree: Tree[A]): Int = {
+    tree match {
+      case Branch(l, r) => 1 + Integer.max(depth(l), depth(r))
+      case _ => 0
+    }
+  }
+
+
   // TODO: 3.28
   // TODO: 3.29
 }
 
 object TreeMain extends App {
-  val treeTest = Branch(Leaf(3), Branch(Leaf(2), Leaf(1)))
-  println(Tree.size(treeTest, 0))
+
+  val leafTest = Leaf(1)
+  val treeTest = Branch(Branch(Leaf(3), Leaf(1)), Leaf(8))
+  val treeTest1 = Branch(Branch(Leaf(3), Leaf(1)), Branch(Leaf(8), Leaf(6)))
+  val treeTest2 = Branch(Branch(Leaf(10), Leaf(235)), Branch(Leaf(8), Branch(Leaf(9), Leaf(1000))))
+  val treeTest3 = Branch(Leaf(3), Leaf(1))
+  val treeTest4 = Branch(Branch(Leaf(10), Leaf(235)), Branch(Leaf(8), Branch(Leaf(9), Branch(Leaf(8), Branch(Leaf(9), Leaf(1000))))))
+
+  println(Tree.size(treeTest1))
+
+  println(Tree.maxInTree(treeTest2))
+
+
+  println(Tree.depth(leafTest)) // 1
+  println(Tree.depth(treeTest3)) // 2
+  println(Tree.depth(treeTest)) // 3
+  println(Tree.depth(treeTest1)) // 3
+  println(Tree.depth(treeTest2)) // 4
+  println(Tree.depth(treeTest4)) // 6
+
+
+
 }
