efa2d89493cfec66d1ce1adafdc098de10f312c2
ooussem <oussem93@hotmail.fr>
2021-04-29 20:56:09 +0200

    Add ex 4.7

--- src/main/scala/com/book/fpinscala/chap4/Either.scala
+++ src/main/scala/com/book/fpinscala/chap4/Either.scala
@@ -30,8 +30,38 @@ sealed trait Either[+E, +A]{
     for{ aValue <- this; bValue <- bEither } yield f(aValue, bValue)
   }
 
+}
+
+object EitherComp {
+
+  // Add ex 4.7
+  def sequence[E, A](listEither: List[Either[E, A]]): Either[E, List[A]] = {
+    listEither match {
+      case head :: tail => head.flatMap(hh => sequence(tail).map(tt => hh :: tt))
+      case Nil => Right(Nil)
+    }
+
+    listEither match {
+      case head :: tail => head.map2(sequence(tail))(_ :: _)
+      case Nil => Right(Nil)
+    }
+
+    listEither.foldRight(Right(List[A]()): Either[E, List[A]])((e, l) => e.map2(l)(_ :: _))
+  }
+
+  def traverse[E, A, B](list: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
+    list match {
+//      case head :: tail => f(head).flatMap(hh => traverse(tail)(f).map(tt => hh :: tt))
+      case head :: tail => f(head).map2(traverse(tail)(f))(_ :: _)
+      case Nil => Right(Nil)
+    }
+
+    list.foldRight(Right(List[B]()): Either[E, List[B]])((a, b) => f(a).map2(b)(_ :: _))
+  }
+
 
 
 }
+
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
--- src/test/scala/com/book/fpinscala/chap4/EitherTest.scala
+++ src/test/scala/com/book/fpinscala/chap4/EitherTest.scala
@@ -13,8 +13,9 @@ class EitherTest extends AnyFunSuite {
   }
 
   test("map() Either Left test") {
-    val eitherTest: Either[Exception, Int] = Left(new Exception("Left !!"))
-    val leftExpected: Either[Exception, Int] = Left(new Exception("Left !!"))
+    val exceptionTest = new Exception("Left !!")
+    val eitherTest: Either[Exception, Int]    = Left(exceptionTest)
+    val leftExpected: Either[Exception, Int]  = Left(exceptionTest)
 
     eitherTest.map(_ * 2) shouldEqual leftExpected
   }
@@ -38,6 +39,22 @@ class EitherTest extends AnyFunSuite {
     val rightExpected: Either[Exception, Double] = Right(8)
 
     eitherTest.map2(otherEither)(functionForTest) shouldEqual rightExpected
+  }
+
+  test("sequence() Either test w/o error") {
+    val eitherTest: List[Either[Exception, Int]] = List(Right(4), Right(8))
+    val expectedResult: Either[Exception, List[Int]] = Right(List(4, 8))
+
+    EitherComp.sequence(eitherTest) shouldEqual expectedResult
+  }
+
+  test("traverse()") {
+    val eitherTest: List[Int] = List(4, 8)
+    val functionTest: Int => Either[Exception, String] = i => Right(i.toString)
+
+    val expectedResult: Either[Exception, List[String]] = Right(List("4", "8"))
+
+    EitherComp.traverse(eitherTest)(functionTest) shouldEqual expectedResult
   }
 
 }
